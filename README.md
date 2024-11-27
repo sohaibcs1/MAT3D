1a5efbe520c6ed699144c1311f797e384c44a02d
Muhammad Sohaib <43402659+sohaibcs1@users.noreply.github.com>
2024-11-26 17:25:47 -0800

    added

---
+++ README.md
@@ -0,0 +1,132 @@
+
+# Multi-Aperture Transformers for 3D (MAT3D) Segmentation of Clinical and Microscopic Images
+
+🚀 **Official PyTorch Implementation**  
+
+
+---
+
+## 📜 Overview
+
+MAT3D uses Multi-Aperture Transformers for accurate 3D segmentation in clinical and microscopic imaging
+
+<div align="center" style="display: flex; flex-direction: row; justify-content: center; align-items: center; gap: 20px;">
+  <div>
+    <a href="gif/10a_day2_c.gif">
+      <img src="gif/10a_day2_c.gif" alt="Video Preview 1" width="200px" />
+    </a>
+    <p style="text-align: center;">MCF10A Day 2</p>
+  </div>
+  <div>
+    <a href="gif/10a_day5_c.gif">
+      <img src="gif/10a_day5_c.gif" alt="Video Preview 2" width="200px" />
+    </a>
+    <p style="text-align: center;">MCF10A Day 5</p>
+  </div>
+  <div>
+    <a href="gif/10a_day7.gif">
+      <img src="gif/10a_day7.gif" alt="Video Preview 3" width="200px" />
+    </a>
+    <p style="text-align: center;">MCF10A Day 7</p>
+  </div>
+</div>
+
+
+
+
+---
+### 🌌  Framework
+<div align="center">
+  <img src="diagram/framework.png" alt="MAT3D Framework" style="width:80%;"/>
+</div>
+
+---
+
+## 🛠️ Environment Setup
+
+Set up the environment for MAT3D as follows:
+
+```bash
+python3.10 -m venv MAT3D_env 
+source MAT3D_env/bin/activate 
+pip install -r requirements.txt
+```
+
+---
+
+## 📂 Dataset
+
+### 🔗 Download Links:
+- [Synapse Multi-Organ Dataset](https://www.synapse.org/#!Synapse:syn3193805/wiki/89480)  
+- [ACDC Challenge Dataset](https://www.creatis.insa-lyon.fr/Challenge/acdc/)
+
+### 🗂️ Folder Structure:
+Organize your data as follows:
+
+```
+data/  
+├── imagesTr/  
+│   ├── img1.nii.gz  
+│   ├── img2.nii.gz  
+├── labelsTr/  
+│   ├── label1.nii.gz  
+│   ├── label2.nii.gz  
+├── dataset.json  
+```
+
+---
+
+## 🚀 Running the Code
+
+This repository is built upon the foundational work provided in [Synapse](https://github.com/LeonidAlekseev/Swin-UNETR).
+
+### Training
+
+Before training, configure the hyperparameters in the `config.py` file:
+
+#### 🔧 Hyperparameter Configuration:
+- **`data_dir`**: Path to the dataset.  
+- **`saved_model_dir`**: Directory to save trained models and checkpoints.  
+- **`num_samples`**: Number of samples for training.  
+- **`num_classes`**: Number of target classes + background.  
+- **`input_size`**: Dimensions of input images/data.  
+- **`input_channels`**: Number of input channels (e.g., grayscale=1, RGB=3).  
+- **`feature_size`**: Size of feature vectors extracted by the model.  
+- **`use_checkpoint`**: Enable/disable model checkpointing.  
+- **`learning_rate`**: Initial learning rate.  
+- **`weight_decay`**: L2 penalty rate for regularization.  
+- **`max_iterations`**: Maximum number of training iterations.  
+- **`eval_num`**: Frequency of evaluations during training.
+
+#### Start Training:
+```bash
+python3.10 main.py  
+```
+
+---
+
+## 📊 Results
+
+### Quantitative Results (Microscopic data):
+<div align="center">
+  <img src="diagram/numeric_results.png" alt="Quantitative Results" style="width:80%;"/>
+</div>
+
+### Clinical data Visualization:
+<div align="center">
+  <img src="diagram/visual_res.png" alt="Visualization Results" style="width:80%;"/>
+</div>
+
+### Microscopic data Visualization:
+
+<div align="center">
+  <img src="diagram/visual_res2.png" alt="Visualization Results" style="width:80%;"/>
+  
+</div>
+
+
+## 📣 Citation
+
+If any part of this repository is helpful in your research, please consider citing our paper.
+
+---
---
+++ train/utils.py
@@ -0,0 +1,110 @@
+from monai.transforms import MapTransform
+import os
+import shutil
+import tempfile
+import torch
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from monai.losses import DiceCELoss
+from monai.inferers import sliding_window_inference
+from monai.transforms import (
+    AsDiscrete,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandFlipd,
+    RandCropByPosNegLabeld,
+    RandShiftIntensityd,
+    ScaleIntensityRanged,
+    Spacingd,
+    RandRotate90d,
+    EnsureTyped,
+)
+
+from monai.config import print_config
+from monai.metrics import DiceMetric
+from monai.data import (
+    ThreadDataLoader,
+    CacheDataset,
+    load_decathlon_datalist,
+    decollate_batch,
+    set_track_meta,
+)
+# train the model
+from monai.transforms import AsDiscrete
+from monai.metrics import DiceMetric
+
+
+def validation(model, epoch_iterator_val, config, global_step):
+    post_label = AsDiscrete(to_onehot=config["num_classes"])
+    post_pred = AsDiscrete(argmax=True, to_onehot=config["num_classes"])
+    dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False)
+
+    model.eval()
+    with torch.no_grad():
+        dice = []
+        for batch in epoch_iterator_val:
+            val_inputs, val_labels = (batch["image"].cuda(), config["input_size"].cuda())
+            with torch.cuda.amp.autocast():
+                val_outputs = sliding_window_inference(val_inputs, (96, 96, 96), 1, model)
+            val_labels_list = decollate_batch(val_labels)
+            val_labels_convert = [post_label(val_label_tensor) for val_label_tensor in val_labels_list]
+            val_outputs_list = decollate_batch(val_outputs)
+            val_output_convert = [post_pred(val_pred_tensor) for val_pred_tensor in val_outputs_list]
+            a= dice_metric(y_pred=val_output_convert, y=val_labels_convert)
+            epoch_iterator_val.set_description("Validate (%d / %d Steps)" % (global_step, 10.0))
+            dice.append(a)
+        mean_dice_val = dice_metric.aggregate().item()
+        print("mean dice score:", torch.nanmean(torch.stack(dice), dim=0))
+        dice_metric.reset()
+    return mean_dice_val
+
+
+def train(model, global_step, train_loader,val_loader,config, dice_val_best, global_step_best):
+
+    # loss function and optimizer
+    torch.backends.cudnn.benchmark = True
+    loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config["learning_rate"], weight_decay=config["weight_decay"])
+    scaler = torch.cuda.amp.GradScaler()
+
+    ## start train the model
+    model.train()
+    epoch_loss = 0
+    step = 0
+    epoch_iterator = tqdm(train_loader, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True)
+    for step, batch in enumerate(epoch_iterator):
+        step += 1
+        x, y = (batch["image"].cuda(), batch["label"].cuda())
+        with torch.cuda.amp.autocast():
+            logit_map = model(x)
+            loss = loss_function(logit_map, y)
+        scaler.scale(loss).backward()
+        epoch_loss += loss.item()
+        scaler.unscale_(optimizer)
+        scaler.step(optimizer)
+        scaler.update()
+        optimizer.zero_grad()
+        epoch_iterator.set_description(f"Training ({global_step} / {config['max_iterations']} Steps) (loss={loss:2.5f})")
+        if (global_step % config["eval_num"] == 0 and global_step != 0) or global_step == config["max_iterations"]:
+            epoch_iterator_val = tqdm(val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True)
+            dice_val = validation(epoch_iterator_val, config, global_step)
+            epoch_loss /= step
+            # epoch_loss_values.append(epoch_loss)
+            # metric_values.append(dice_val)
+            if dice_val > dice_val_best:
+                dice_val_best = dice_val
+                global_step_best = global_step
+                torch.save(model.state_dict(), os.path.join(config["saved_model_dir"], "best_model.pth"))
+                print(
+                    "Model Was Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(dice_val_best, dice_val)
+                )
+            else:
+                print(
+                    "Model Was Not Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
+                        dice_val_best, dice_val
+                    )
+                )
+        global_step += 1
+    return global_step, dice_val_best, global_step_best
